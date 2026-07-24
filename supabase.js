bcedbb5fe7aca23d40bc36502e56a9947b9d57ee
iqbalkhan11296-coder <iqbalkhan11296@gmail.com>
2026-07-24 19:56:44 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const supabaseUrl = "https://sshxlhximkhquyzvwqyw.supabase.co";
 
-const supabaseKey = "sb_publishable_sBwFTPgrkAl_-AjVdRosVA_kalI9hPj";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzaHhsaHhpbWtocXV5enZ3cXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4ODU4ODksImV4cCI6MjEwMDQ2MTg4OX0.nlWwcGHSZhlUE1noVMbJnkaRvNiarCJjQvoqtAtlRQ4";
 
 const supabase = window.supabase.createClient(
     supabaseUrl,
