bdf9d62d39c2e10810af23693cf471ec736615d5
iqbalkhan11296-coder <iqbalkhan11296@gmail.com>
2026-07-24 16:38:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -111,3 +111,30 @@ document.getElementById("checkout-btn").onclick = function(){
 }
 
 renderCart();
+// Search Menu
+
+function searchMenu(){
+
+    let input = document
+        .getElementById("search")
+        .value
+        .toLowerCase();
+
+    let cards = document
+        .querySelectorAll(".card");
+
+    cards.forEach(card=>{
+
+        let name = card
+            .querySelector("h2")
+            .innerText
+            .toLowerCase();
+
+        if(name.includes(input))
+            card.style.display="block";
+        else
+            card.style.display="none";
+
+    });
+
+}
