34f83cd14fd49e3fbc8e59aa0ee63b05d8637b3e
iqbalkhan11296-coder <iqbalkhan11296@gmail.com>
2026-07-24 16:55:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -138,3 +138,26 @@ function searchMenu(){
     });
 
 }
+function filterMenu(category){
+
+const cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+if(category==="all"){
+
+card.style.display="block";
+
+}else if(card.classList.contains(category)){
+
+card.style.display="block";
+
+}else{
+
+card.style.display="none";
+
+}
+
+});
+
+}
